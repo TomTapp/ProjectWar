f579b9295823dcb2f8902d961ec9539c3750f981
Brett Ursprung <brett.ursprung@gmail.com>
2021-07-14 09:47:44 -0500

    removed extra 6

--- deck/deck.js
+++ deck/deck.js
@@ -1,9 +1,9 @@
 var suits = ["H", "C", "D", "S"]
 
-var ranks = ["2", "3","4", "5", "6", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+var ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
 
 for(var suitCounter = 0; suitCounter < 4; suitcounter = suitCounter++) {
-    for (var rankCounter = 0; rankCounter < 14; rankCounter++){
+    for (var rankCounter = 0; rankCounter < 13; rankCounter++){
         console.log(ranks[rankCounter] + suits[suitCounter]);
     }
 }
