5a86c20b74e40848458b2a07ddc212190f514093
Brett Ursprung <brett.ursprung@gmail.com>
2021-07-14 09:40:12 -0500

    card deck

--- deck/deck.js
+++ deck/deck.js
@@ -2,10 +2,9 @@ var suits = ["H", "C", "D", "S"]
 
 var ranks = ["2", "3","4", "5", "6", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
 
-for(var suitCounter = 0; suitCounter < 4; suitcounter = suitCounter + 1) {
-    console.log(suits[suitCounter]);
+for(var suitCounter = 0; suitCounter < 4; suitcounter = suitCounter++) {
+    for (var rankCounter = 0; rankCounter < 13; rankCounter++){
+        console.log(ranks[rankCounter] + suits[suitCounter]);
+    }
 }
 
-for(var rankCounter = 0; rankCounter < 13; rankCounter + 1){
-    console.log(ranks[rankCounter]);
-}
