b6f72f5435c9dd2cf3fa9b4982fe91d7b96cdc6e
Jaime J Cobo <jaimecobo@outlook.com>
2021-07-14 09:56:31 -0600

    New folder assets, moved ProjectWar.js to assets, also defined some classes and defined some structure

---
+++ assets/ProjectWar.js
@@ -0,0 +1,45 @@
+class Game{
+    constructor(gameName){
+        thisgameName = gameName;
+    }
+
+    shuffle(players){
+        // Creates  a collection of cards conformed of the amount of decks based on the amount of players
+            for(let i = 0; i < players.length; i++){
+        
+            }
+        
+        }
+
+}
+
+    function shuffle(players){
+// Creates  a collection of cards conformed of the amount of decks based on the amount of players
+    for(let i = 0; i < players.length; i++){
+
+    }
+
+}
+
+}
+
+class War extends Game{
+    constructor(){
+
+    }
+
+
+}
+
+class Payer {
+    constructor(name, score) {
+      this.name = name;
+      this.score = score;
+    }
+  }
+
+
+function players(numOfPlayers){
+    // Creates a new player
+
+}
