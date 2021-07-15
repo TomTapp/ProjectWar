562b0ad9c8ec65af0a482ea7330cb6b0a918bacb
stodd3 <7934735+stodd3@users.noreply.github.com>
2021-07-15 10:27:34 -0500

    Update deck.js

--- deck/deck.js
+++ deck/deck.js
@@ -1,20 +1,6 @@
 deck = function () {
   let suits = ['H', 'C', 'D', 'S'];
-  let ranks = [
-    '2',
-    '3',
-    '4',
-    '5',
-    '6',
-    '7',
-    '8',
-    '9',
-    '10',
-    'J',
-    'Q',
-    'K',
-    'A',
-  ];
+ let ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
   let deck = [];
   for (let suitCounter = 0; suitCounter < 4; suitcounter = suitCounter++) {
     for (let rankCounter = 0; rankCounter < 13; rankCounter++) {
