e538b80c3d07cb64a85c95fd4ac336fe9817dd00
stodd3 <saratodd3@gmail.com>
2021-07-15 10:21:43 -0500

    created a function for deck to return array git: 'add.' is not a git command. See 'git --help

--- deck/deck.js
+++ deck/deck.js
@@ -1,17 +1,30 @@
+/** @format */
 
-
-var suits = ["H", "C", "D", "S"];
-var ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
-
-var deck = [];
-
-
-for (var suitCounter = 0; suitCounter < 4; suitcounter = suitCounter++) {
-  for (var rankCounter = 0; rankCounter < 13; rankCounter++) {
-    console.log(ranks[rankCounter] + suits[suitCounter]);
-deck.push(ranks[rankCounter] + suits[suitCounter])
+deck = function () {
+  let suits = ['H', 'C', 'D', 'S'];
+  let ranks = [
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+    '10',
+    'J',
+    'Q',
+    'K',
+    'A',
+  ];
+  let deck = [];
+  for (let suitCounter = 0; suitCounter < 4; suitcounter = suitCounter++) {
+    for (let rankCounter = 0; rankCounter < 13; rankCounter++) {
+      // console.log(ranks[rankCounter] + suits[suitCounter]);
+      deck.push(ranks[rankCounter] + suits[suitCounter]);
+    }
   }
-}
+  return deck;
+};
 
-console.log(deck)
-
+// console.log(deck());
